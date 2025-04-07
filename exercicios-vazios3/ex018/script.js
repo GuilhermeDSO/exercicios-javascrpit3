b598fdd151b7170283251587e871fa67981d0d3a
Guilherme <guisoliveira1515@gmail.com>
2025-04-07 19:30:29 -0300

    Add files via upload

---
+++ exercicios-vazios3/ex018/script.js
@@ -0,0 +1,13 @@
+
+var letra = prompt("Digite uma letra:");
+
+
+if(letra == letra.toLocaleUpperCase()){
+    alert("Letra Maiscula")
+}else{
+  alert("Letra Minuscula")
+
+}
+            
+
+alert("A letra é: " + tipoLetra);
